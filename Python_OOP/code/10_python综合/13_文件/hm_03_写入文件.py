1b536cc85371b11863b76f7ef944f3e8ce347e3a
wangzhenguang <2215566216@qq.com>
2023-08-01 15:50:23 +0800

    测试模块修改

--- Python_OOP/code/10_python综合/13_文件/hm_03_写入文件.py
+++ Python_OOP/code/10_python综合/13_文件/hm_03_写入文件.py
@@ -2,7 +2,14 @@
 file = open("README", "a")
 
 # 2. 写入文件
-file.write("123 hello")
+file.write("\n123 hello")
 
 # 3. 关闭
+file.close()
+
+file = open("README")
+
+text = file.read()
+print(text)
+
 file.close()
